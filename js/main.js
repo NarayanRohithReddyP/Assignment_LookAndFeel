06bd8eb10cf7fe16f4b407653659ab8efdf0dba8
Chakravarthy Piniganti <chakravarthy26@usf.edu>
2024-04-19 16:51:04 -0400

    request page working draft

--- js/main.js
+++ js/main.js
@@ -2,13 +2,24 @@
  * Your JS code here
  */
 
+document.addEventListener("DOMContentLoaded", function () {
+    var startButton = document.getElementById("start-button");
+    var form = document.getElementById("fundraising-form");
+
+    startButton.addEventListener("click", function () {
+        form.style.display = "block";
+        startButton.style.display = "none";
+    });
+});
+
+
 /**
  * Sample code
  */
-document.addEventListener('DOMContentLoaded', function() {
-  var ulElement = document.getElementById('links');
-  var liElement = document.createElement('li');
-  liElement.appendChild(document.createTextNode('created by main.js'));
+document.addEventListener('DOMContentLoaded', function () {
+    var ulElement = document.getElementById('links');
+    var liElement = document.createElement('li');
+    liElement.appendChild(document.createTextNode('created by main.js'));
 
-  ulElement.appendChild(liElement);
+    ulElement.appendChild(liElement);
 });
